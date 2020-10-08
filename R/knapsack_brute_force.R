5b2e8e022d9a5be69e3f1b1a172ac5d064fb277f
nicta839 <nicolas.juha.taba@gmail.com>
2020-10-08 09:33:05 +0200

    brute force needs more work

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -15,33 +15,51 @@ brute_force_knapsack <- function(x, W){
   # use the combn function to calculate permutations
   #smallest permutations should start at element 2
   i <- 2
-  
+
   while(i <= nrow(x)){
     #get all combinations for weights and values in matrix of 2 rows
     weight_comb <- combn(x[,"w"], i)
     value_comb <- combn(x[,"v"], i)
+
     #sum all columns to get the weight and value sums
     #returns vectors
     sum_weight_comb <- colSums(weight_comb)
     sum_value_comb <- colSums(value_comb)
-    
+
     #find indices of correct weight combinations
     indices <- which(sum_weight_comb <= W)
-    
+
     #finds and saves the max value and elements
     value <- max(sum_value_comb[indices])
     index_max <- which.max(sum_value_comb[indices])
-    
-    elements_weight_temp <- weight_comb[, index_max]
-    
+
+    elements_weight <- weight_comb[, index_max]
+
     for(j in 1:i){
-      elements[j] <- which(x[,"w"] == elements_weight_temp[j])
+      elements[j] <- which(x[,"w"] == elements_weight[j])
       j <- j + 1
     }
     i <- i + 1
   }
   
   
+    # Trial, doesn't work
+    # elements_temp <- unlist(lapply(1:nrow(x), function(i){
+    #   combn(rownames(x), m = i)
+    # }))
+    # 
+    # 
+    # value_temp <- lapply(elements_temp, function(elements_temp){
+    #   ifelse(sum(x[elements_temp, "w"]) <= W,
+    #          sum(x[elements_temp, "v"]),
+    #              0)
+    #   })
+    # 
+    # 
+    # value <- value_temp[[which.max(value_temp)]]
+    # elements <- elements_temp[[which.max(value_temp)]]
+
+
   result <- list("value" = value, "elements" = elements)
   return(result)
   
