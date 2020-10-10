cbbd34cf42bc364cab937204d17d047a126e3aa9
Yuki <yuki@Yukis-MacBook-Pro-2.local>
2020-10-10 17:24:02 +0200

    brute force implementation

--- R/dynamic_knapsack.R
+++ R/dynamic_knapsack.R
@@ -1,21 +1,3 @@
-# Dynamic programming
-# // Input:
-#   // Values (stored in array v)
-# // Weights (stored in array w)
-# // Number of distinct items (n)
-# // Knapsack capacity (W)
-# // NOTE: The array "v" and array "w" are assumed to store all relevant values starting at index 1.
-# 
-# for j from 0 to W do:
-#   m[0, j] := 0
-# 
-# for i from 1 to n do:
-#   for j from 0 to W do:
-#   if w[i] > j then:
-#   m[i, j] := m[i-1, j]
-# else:
-#   m[i, j] := max(m[i-1, j], m[i-1, j-w[i]] + v[i])
-
 #' @title Dynamic Programming Knapsack
 #' @description Implementation of knapsack problem using dynamic programming.
 #' @references \url{https://en.wikipedia.org/wiki/Knapsack_problem#0.2F1_knapsack_problem}
@@ -56,15 +38,15 @@ knapsack_dynamic <- function(x, W) {
   output_list[["value"]]= round(m[nrow(m),ncol(m)])
   
   items = c()
-  row_num <- nrow(x)+1
-  col_num <- W+1
+  row_num = nrow(x)+1
+  col_num = W+1
   while(row_num > 1){
     if(m[row_num-1, col_num] < m[row_num, col_num] ){
-      items <- append(items, row_num-1)
-      row_num <- row_num - 1
-      col_num <- col_num - weights[row_num]
+      items = append(items, row_num-1)
+      row_num = row_num - 1
+      col_num = col_num - weights[row_num]
     }else{
-      row_num <- row_num - 1
+      row_num = row_num - 1
     }
   }
   
--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -1,66 +1,41 @@
-brute_force_knapsack <- function(x, W){
-  stopifnot(is.data.frame(x))
-  stopifnot(ncol(x)==2)
-  stopifnot(any(x[,c("v","w")] > 0))
+brute_force_knapsack <- function(x, W) {
+  stopifnot(is.numeric(W))
+  if(!"w" %in% colnames(x))
+  {
+    stop("Missing column w in input data");
+  }
+  if(!"v" %in% colnames(x))
+  {
+    stop("Missing column v in input data");
+  }
+  values = x$v
+  weights = x$w
+
+  index_combs = list()
+  weight_combs = list()
+  value_combs = list()
   
-  # Creating necessary vectors
-  value <- 0
-  value_temp <- c()
-  value_temp[1] <- 0
-  elements <- c()
-  elements_temp <- c()
-  elements_temp[1] <- 0
-  weight <- 0
-  
-  # use the combn function to calculate permutations
-  #smallest permutations should start at element 2
-  i <- 2
-
-  while(i <= nrow(x)){
-    #get all combinations for weights and values in matrix of 2 rows
-    weight_comb <- combn(x[,"w"], i)
-    value_comb <- combn(x[,"v"], i)
-
-    #sum all columns to get the weight and value sums
-    #returns vectors
-    sum_weight_comb <- colSums(weight_comb)
-    sum_value_comb <- colSums(value_comb)
-
-    #find indices of correct weight combinations
-    indices <- which(sum_weight_comb <= W)
-
-    #finds and saves the max value and elements
-    value <- max(sum_value_comb[indices])
-    index_max <- which.max(sum_value_comb[indices])
-
-    elements_weight <- weight_comb[, index_max]
-
-    for(j in 1:i){
-      elements[j] <- which(x[,"w"] == elements_weight[j])
-      j <- j + 1
-    }
-    i <- i + 1
+  # Iterate over each possible number of element combinations (1-n) and fill list
+  for(i in 1:nrow(x)){
+    index_combs[[i]] = combn(rownames(x), i, paste, collapse=",")
+    weight_combs[[i]] = combn(weights, i, sum)
+    value_combs[[i]] = combn(values, i, sum)
   }
   
+  # Transform list to vector for index, weight and value combinations
+  index_combs_v = unlist(index_combs)
+  weight_combs_v = unlist(weight_combs)
+  value_combs_v = unlist(value_combs)
   
-    # Trial, doesn't work
-    # elements_temp <- unlist(lapply(1:nrow(x), function(i){
-    #   combn(rownames(x), m = i)
-    # }))
-    # 
-    # 
-    # value_temp <- lapply(elements_temp, function(elements_temp){
-    #   ifelse(sum(x[elements_temp, "w"]) <= W,
-    #          sum(x[elements_temp, "v"]),
-    #              0)
-    #   })
-    # 
-    # 
-    # value <- value_temp[[which.max(value_temp)]]
-    # elements <- elements_temp[[which.max(value_temp)]]
-
-
-  result <- list("value" = value, "elements" = elements)
-  return(result)
+  # Create index mask to figure out which weights are below threshold
+  mask_weight = which(weight_combs_v <= W)
+  filtered_value_combs = value_combs_v[mask_weight]
+  value = max(filtered_value_combs)
   
-}
+  # Create index mask to figure out which value combination is the optimum
+  # Unlist and transform to create valid output format for elements
+  mask_values = which(value_combs_v == value)
+  elements = as.numeric(unlist(strsplit(index_combs_v[mask_values],",")))
+  
+  return(list(value=round(value), elements=elements))
+}  
