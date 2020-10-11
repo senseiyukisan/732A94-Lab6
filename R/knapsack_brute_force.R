9ed591b456ad42c2cd25e33bc8e170e520618bf7
nicta839 <nicolas.juha.taba@gmail.com>
2020-10-11 17:16:56 +0200

    parallel brute force implemented

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -1,4 +1,4 @@
-brute_force_knapsack <- function(x, W) {
+brute_force_knapsack <- function(x, W, parallel = FALSE) {
   stopifnot(is.numeric(W))
   if(!"w" %in% colnames(x))
   {
@@ -15,13 +15,30 @@ brute_force_knapsack <- function(x, W) {
   weight_combs = list()
   value_combs = list()
   
-  # Iterate over each possible number of element combinations (1-n) and fill list
-  for(i in 1:nrow(x)){
-    # Using paste function to sum index strings together
-    index_combs[[i]] = combn(rownames(x), i, paste, collapse=",")
-    # Using sum function to sum weights and values
-    weight_combs[[i]] = combn(weights, i, sum)
-    value_combs[[i]] = combn(values, i, sum)
+  if(parallel == FALSE){
+    # Iterate over each possible number of element combinations (1-n) and fill list
+    for(i in 1:nrow(x)){
+      # Using paste function to sum index strings together
+      index_combs[[i]] = combn(rownames(x), i, paste, collapse=",")
+      # Using sum function to sum weights and values
+      weight_combs[[i]] = combn(weights, i, sum)
+      value_combs[[i]] = combn(values, i, sum)
+    }
+    
+  }else{
+      cores <- parallel::detectCores()
+      cl <- parallel::makeCluster(cores, type = "PSOCK")
+      parallel::clusterExport(cl=cl, varlist=c("x"), envir=environment())
+      index_combs <- parallel::parLapply(cl, 1:length(x$v), function(i){
+        combn(rownames(x), m = i, paste, collapse=",")
+      })
+      weight_combs <- parallel::parLapply(cl, 1:length(x$w),function(i){
+        combn(weights, m = i, sum) 
+      })
+      value_combs <- parallel::parLapply(cl, 1:length(x$v), function(i){
+        combn(values, m = i, sum) 
+      })
+      parallel::stopCluster(cl)
   }
   
   # Transform list to vector for index, weight and value combinations
@@ -37,7 +54,8 @@ brute_force_knapsack <- function(x, W) {
   # Create index mask to figure out which value combination is the optimum
   # Unlist and transform to create valid output format for elements
   mask_values = which(value_combs_v == value)
-  elements = as.numeric(unlist(strsplit(index_combs_v[mask_values],",")))
+  elements = as.numeric(unlist(strsplit(index_combs_v[mask_values],",")))  
+  
   
   return(list(value=round(value), elements=elements))
 }  
